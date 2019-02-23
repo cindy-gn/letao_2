2133b2dd06c62ed55b9b87112295d142013cf71e
cindy-gn <464957784@qq.com>
2019-02-23 18:38:17 +0800

    数据可视化完成

---
+++ public/back/js/index.js
@@ -0,0 +1,88 @@
+
+
+$(function () {
+    // 基于准备好的dom，初始化echarts实例
+    var echarts_left = echarts.init(document.querySelector(".echarts_left"));
+
+    // 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: '2019年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data: ['人数','销量']
+        },
+        xAxis: {
+            data: ["1月", "2月", "3月", "4月", "5月", "6月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [500, 202, 360, 399, 400, 520]
+        },
+        {
+            name: '销量',
+            type: 'bar',
+            data: [50, 102, 260, 309, 300, 420]
+        }
+        ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    echarts_left.setOption(option1);
+
+
+    var echarts_right = echarts.init(document.querySelector(".echarts_right"));
+    var option2 = {
+        title: {
+            text: '热门品牌销售',
+            subtext: '2019年2月',
+            x: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['阿迪', '耐克', '新百伦', '老北京', '李宁']
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    { value: 335, name: '阿迪' },
+                    { value: 310, name: '耐克' },
+                    { value: 234, name: '新百伦' },
+                    { value: 135, name: '老北京' },
+                    { value: 1548, name: '李宁' }
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 20,
+                        shadowOffsetX: 0,
+                        shadowColor: 'yellow'
+                    }
+                }
+            }
+        ]
+    };
+
+
+    echarts_right.setOption(option2);
+
+
+
+
+
+
+
+
+
+})
